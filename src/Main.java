b8bf94ea1963dce49555826b98a2e6c6da1a61d4
ricosar <ricos@sheridancollege.ca>
2025-09-24 20:15:14 -0400

    Add second greeting to Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     
     public static void main(String[] args){
         System.out.println("Hello git workflow in java!");
+        System.out.println("This is a new greeting feature!");
     }
 }
